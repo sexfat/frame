e7ae707e167a5c2cdb9a7a09964a14cf742198c9
bryan <bryant6831@gmail.com>
2016-03-29 18:02:08 +0800

    redesin js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,90 +1,102 @@
-module.exports = function (grunt) {
-    grunt.initConfig({
+module.exports = function(grunt) {
+  grunt.initConfig({
 
 
-      //jade
-      // jade: {
-      //     compile: {
-      //         options: {
-      //             client: false,
-      //             pretty: true
-      //         },
-      //         files: [ {
-      //           cwd: 'views/htmls',
-      //           src: '*.jade',
-      //           dest: 'views/',
-      //           expand: true,
-      //           ext: 'main.html'
-      //         } ]
-      //     }
-      // },
+    //jade
+    // jade: {
+    //     compile: {
+    //         options: {
+    //             client: false,
+    //             pretty: true
+    //         },
+    //         files: [ {
+    //           cwd: 'views/htmls',
+    //           src: '*.jade',
+    //           dest: 'views/',
+    //           expand: true,
+    //           ext: 'main.html'
+    //         } ]
+    //     }
+    // },
 
-      jade: {
-          compile: {
-            options: {
-              data: {
-                debug: false
-              }
-            },
-            files: {
-              "views/main.html": ["views/htmls/*.jade"]
-            }
+    jade: {
+      compile: {
+        options: {
+          data: {
+            debug: false
           }
         },
-
-      //watch
-        watch: {
-          css: {
-            files: ['views/sass/*.scss','views/sass/**/*.scss'],
-            tasks: ['sass']
-           },
-           html: {
-             files: ['views/htmls/main.jade'],
-             tasks: ['jade']
-         }
-       },
-      //sass
-        sass: {
-            build: {
-                options: {
-                    sourcemap: 'none'
-                     },
-            files: [{
-                     expand: true,
-                     cwd: 'views/sass/',
-                     src: ['*.scss'],
-                     dest: 'views/css/',
-                     ext: '.css'
-                      }]
-                   }
-          },
-      //  browserSync
-        browserSync: {
-            dev: {
-                bsFiles: {
-                    src : [
-                        'views/css/*.css',
-                        'views/*.html'
-                    ]
-                },
-                options: {
-                    watchTask: true,
-                    server: {
-                      baseDir: './views',
-                      index: 'index.html'
-                    }
-                }
-            }
+        files: {
+          "views/main.html": ["views/htmls/*.jade"]
         }
-    });
+      }
+    },
 
-    // load npm tasks
+    //watch
+    watch: {
+      css: {
+        files: ['views/sass/*.scss', 'views/sass/**/*.scss'],
+        tasks: ['sass']
+      },
+      html: {
+        files: ['views/htmls/main.jade'],
+        tasks: ['jade']
+      },
+      js: {
+        files: ['views/js/*.js']
+      }
+    },
+    //sass
+    sass: {
+      build: {
+        options: {
+          sourcemap: 'none'
+        },
+        files: [{
+          expand: true,
+          cwd: 'views/sass/',
+          src: ['*.scss'],
+          dest: 'views/css/',
+          ext: '.css'
+        }]
+      }
+    },
+    //jshint
+    jshint: {
+      all: {
+        src: ['views/libs/*.js', 'views/js/*.js'],
+      },
+    },
 
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-browser-sync');
-    grunt.loadNpmTasks('grunt-contrib-jade');
+    //  browserSync
+    browserSync: {
+      dev: {
+        bsFiles: {
+          src: [
+            'views/css/*.css',
+            'views/*.html',
+            'views/js/*.js'
+          ]
+        },
+        options: {
+          watchTask: true,
+          server: {
+            baseDir: './views',
+            index: 'index.html'
+          }
+        }
+      }
+    }
+  });
 
-    // define default task
-    grunt.registerTask('default', ['browserSync', 'watch' ,'jade']);
+  // load npm tasks
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browser-sync');
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // define default task
+  grunt.registerTask('default', ['browserSync', 'watch', 'jade', 'jshint']);
 };
