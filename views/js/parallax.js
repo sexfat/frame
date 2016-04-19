07d06bc84f7eb0247b0fb95c59fe7d2db83ccab9
bryan <bryant6831@gmail.com>
2016-04-19 17:14:38 +0800

    update parallax

--- views/js/parallax.js
+++ views/js/parallax.js
@@ -1,6 +1,3 @@
-
-
-
 // $(document).ready(function() {
 //
 // 	redrawDotNav();
@@ -85,16 +82,16 @@
 //
 // }
 window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
-window.webkitRequestAnimationFrame || window.oRequestAnimationFrame;
+      window.webkitRequestAnimationFrame || window.oRequestAnimationFrame;
 var body = document.querySelector('.parallax'),
       scrollTop = 0;
-var lv0 = body.querySelector('.parallax__layer--deep'),
-      lv1 = body.querySelector('.parallax__layer--back'),
-      lv2 = body.querySelector('.parallax__layer--base')
-lv3 = body.querySelector('.parallax__layer--fore');
+var   lv0 = body.querySelector('.parallax__layer-deep'),
+      lv1 = body.querySelector('.parallax__layer–back'),
+      lv2 = body.querySelector('.parallax__layer–base'),
+      lv3 = body.querySelector('.parallax__layer-fore');
 body.addEventListener('scroll', function(e) {
       scrollTop = body.scrollTop;
       lv0.style.top = parseInt(scrollTop * 0.333) + 'px';
-      lv2.style.top = (scrollTop * -1) + 'px';
-      lv3.style.top = (scrollTop * -2) + 'px';
+      lv2.style.top = (scrollTop*-1) + 'px';
+      lv3.style.top = (scrollTop*-2) + 'px';
 });
