c2819f5a0968015179eca3ce3ca478220844aa1c
bryan <bryant6831@gmail.com>
2016-04-25 18:31:07 +0800

    update

--- views/js/parallax_02.js
+++ views/js/parallax_02.js
@@ -6,74 +6,95 @@ var controller = new ScrollMagic.Controller();
 // create a scene
 var scene = new ScrollMagic.Scene({
             triggerElement: "#trigger1",
-            duration: 500,
-            offset: 50
+            duration: 230,
+            offset: 250
       })
-      .setTween(TweenMax.staggerTo([".rect", ".rect_02"], 1, {
+      .setTween(TweenMax.staggerTo([".rect", ".rect_02"], 1,  {
             rotation: 180,
-            y: 100
+            y: 100,
+            ease:Linear.easeOut,
       }, 0.5))
-      .addTo(controller);
+
+.addTo(controller);
 
 
 
-
-
-
-// $(window).on('mousemove.parallax', function(event) {
-
-
-// new ScrollMagic.Scene({
-//         triggerElement: "",
-//         duration: 140,
-//         offset: 40
-//     })
-//     .setPin("#my-sticky-element") // pins the element for the the scene's duration
+// var camera, scene, renderer, particles, particle, material, particleCount, points, texture;
+// var xSpeed, ySpeed;
+// xSpeed = 0.0005;
+// ySpeed = 0.001;
+// var winWidth, winHeight;
+// winWidth = window.innerWidth;
+// winHeight = window.innerHeight;
 //
-//     .addTo(controller);
-// console.log('ok');
-
-
-
-
-
-
-
-// var controller = new ScrollMagic.Controller();
+// init();
+// animate();
 //
-//       var scene = new ScrollMagic.Scene({
-//             triggerElement: "#trigger1",
-//             duration: 400,
-//           //   offset: -100
-//         })
-//    .setTween(TweenMax.staggerTo([".rect", ".rect_02"], 1, {
-//           rotation: 180,
-//           y: 100
-//    }, 0.5))
-//    .addTo(controller)
-//       // })
-//       // new ScrollMagic.Scene({
-//       //         triggerElement: "#trigger1",
+// function init() {
+//       scene = new THREE.Scene();
+//       scene.fog = new THREE.FogExp2('#000', 0.001);
 //
-
-
-// });
-
-// TweenMax.staggerFrom( objects ,  1.5 , {left:"100px", opacity:0 , top:"100px", delay:1 , ease: Strong.easeOut});
-
-
-// $(document).ready(function(){
-// });
-
-
-// $('#toggle.buttons a.has-code').click(function() {
+//       camera = new THREE.PerspectiveCamera(75, winWidth / winHeight, 1, 1000);
+//       camera.position.z = 500;
+//
+//       // particle
+//       // transparentとblendingたぶん効いてない
+//       material = new THREE.PointsMaterial({
+//             color: 0xffffff,
+//             size: 3,
+//             transparent: true,
+//             blending: THREE.AdditiveBlending
 //       });
-
-
 //
-// $('#aaa').click(function){
+//       particleCount = 15000;
+//       particles = new THREE.Geometry();
 //
+//       for (var i = 0; i < particleCount; i++) {
+//             var px = Math.random() * 2000 - 1000;
+//             var py = Math.random() * 2000 - 1000;
+//             var pz = Math.random() * 2000 - 1000;
+//             particle = new THREE.Vector3(px, py, pz);
+//             particle.velocity = new THREE.Vector3(0, Math.random(), 0);
+//             particles.vertices.push(particle);
+//       }
 //
+//       points = new THREE.Points(particles, material);
+//       points.sortParticles = true;
+//       scene.add(points);
 //
+//       renderer = new THREE.WebGLRenderer({
+//             antialias: true
+//       });
+//       renderer.setSize(winWidth, winHeight);
+//       renderer.setClearColor('#000', 1);
+//       document.getElementById('canvas').appendChild(renderer.domElement);
+// }
 //
-// });
+// function animate() {
+//       requestAnimationFrame(animate);
+//
+//       scene.rotation. += xSpeed;
+//
+//       // パーティクル上下移動
+//       var i = particleCount;
+//       while (i--) {
+//             var particle = particles.vertices[i];
+//
+//             // y
+//             if (particle.y > 1000) {
+//                   particle.y = -1000;
+//                   particle.velocity.y = Math.random();
+//             }
+//             particle.velocity.y += Math.random() * ySpeed;
+//
+//             particle.add(particle.velocity);
+//       }
+//       points.geometry.verticesNeedUpdate = true;
+//
+//       render();
+// }
+//
+// function render() {
+//       camera.lookAt(scene.position);
+//       renderer.render(scene, camera);
+// }
