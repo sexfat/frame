7db91354d3bda04ea0deab023e415ec7a7d07214
bryan <bryant6831@gmail.com>
2016-04-26 18:29:01 +0800

    update

---
+++ views/js/preload.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+ var	$window = $(window);
+      $body = $('body');
+
+      // Disable animations/transitions until the page has loaded.
+                  $body.addClass('is-loading');
+                   console.log('ok');
+                   $window.on('load', function() {
+                                     window.setTimeout(function() {
+                                            console.log('ok01');
+                                           $body.removeClass('is-loading');
+                                     }, 1000);
+                               });
+});
