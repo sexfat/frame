f804bfe92dae667e52707ac76aa4bbdc33e0cb15
bryan <bryant6831@gmail.com>
2016-06-01 17:54:38 +0800

    add new

--- views/js/nav.js
+++ views/js/nav.js
@@ -1,6 +1,21 @@
-$("a#toggle").on('click',function(e){
-  $('body').toggleClass('js-open');
-  $('nav').toggleClass('js-open');
-  //這不太懂
-  e.preventDefault();
+$("a#toggle").on('click', function(e) {
+      $('body').toggleClass('js-open');
+      $('nav').toggleClass('js-open');
+      //這不太懂
+      e.preventDefault();
+});
+
+
+
+
+$(window).on('scroll', function() {
+
+      var navHeight = $(window).height() - 100;
+      var distance = $(window).scrollTop();
+      if ( distance > navHeight ) {
+             $('.nav').addClass('on');
+      } else() {
+             $('.nav').removeClass('on');
+      }
+
 });
---
+++ views/js/navscroll.js
@@ -0,0 +1,33 @@
+// $(window).on('scroll', function() {
+//
+//       var navHeight = $(window).height() - 10;
+//       var distance = $(window).scrollTop();
+//       console.log('distance');
+//       if ( distance > navHeight ) {
+//              $('.nav').addClass('on');
+//
+//       } else {
+//              $('.nav').removeClass('on');
+//       }
+// console.log('section01');
+// });
+
+
+
+$(document).ready(function() {
+ $('width_all').on('hover' , function(){
+
+
+ });
+});
+
+
+// $(".link").on("mouseenter", function() {
+//   $(this).children(".submenu").show();
+//   $(this).addClass("highlight");
+// });
+//
+// $(".link").on("mouseleave", function() {
+//   $(this).children(".submenu").hide();
+//   $(this).removeClass("highlight");
+});
