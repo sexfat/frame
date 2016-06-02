ba42101e508a1b7c1ca2cca3f6b6d36044643c1c
bryan <bryant6831@gmail.com>
2016-06-02 17:37:39 +0800

    js leftmenu ex

---
+++ views/js/leftmenu.js
@@ -0,0 +1,5 @@
+$('#btn_show').on('click', function() {
+
+$('#navLeft').addClass('show');
+$('.wrapper').addClass('on');
+});
--- views/js/navscroll.js
+++ views/js/navscroll.js
@@ -1,25 +1,27 @@
-// $(window).on('scroll', function() {
-//
-//       var navHeight = $(window).height() - 10;
-//       var distance = $(window).scrollTop();
-//       console.log('distance');
-//       if ( distance > navHeight ) {
-//              $('.nav').addClass('on');
-//
-//       } else {
-//              $('.nav').removeClass('on');
-//       }
-// console.log('section01');
-// });
+$(window).on('scroll', function() {
+
+      var navHeight = $(window).height() - 10;
+      var distance = $(window).scrollTop();
+      console.log('distance');
+      if ( distance > navHeight ) {
+             $('.nav').addClass('on');
+
+      } else {
+             $('.nav').removeClass('on');
+      }
+console.log('section01');
 
 
-
-$(document).ready(function() {
- $('width_all').on('hover' , function(){
-
-
- });
+$('.width_all').on('mouseenter', function() {
+      $(this).children('.submenu').show();
 });
+$('.width_all').on('mouseleave', function() {
+      $(this).children('.submenu').hide();
+});
+});
+
+
+
 
 
 // $(".link").on("mouseenter", function() {
@@ -30,4 +32,4 @@ $(document).ready(function() {
 // $(".link").on("mouseleave", function() {
 //   $(this).children(".submenu").hide();
 //   $(this).removeClass("highlight");
-});
+// });
